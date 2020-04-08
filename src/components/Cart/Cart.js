81f79168e2b69f26c0526527d3c880dd9dd5e650
sarefinsag <shamimularefin100@gmail.com>
2020-04-08 21:52:06 +0600

    ema john mutamuti

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total, prdt) => total + prdt.price, 0);
+
+    let shipping = 0;
+    if(total > 50){
+        shipping = 0;
+    }
+    else if(total > 15){
+        shipping = 4.99;
+    }
+    else if(total > 0){
+        shipping = 12.99;
+    }
+    const tax = (total / 10);
+    const grandTotal = (total + shipping + tax);
+    const formatNumber = num => {
+        const precision = num.toFixed(2);
+        return Number(precision)
+    }
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Product Price: ${formatNumber(total)}</p>
+            <p><small>Shipping Price: ${shipping} </small></p>
+            <p><small>Tax + VAT: ${formatNumber(tax)}</small></p>
+            <p>Total Price: ${formatNumber(grandTotal)} </p>
+        </div>
+    );
+};
+
+export default Cart;
